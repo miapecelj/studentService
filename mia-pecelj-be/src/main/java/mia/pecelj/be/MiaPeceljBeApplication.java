083c8eb07ea3c8cd152975b4014d437ef844c7c7
miapecelj <miapecelj16@gmail.com>
2021-04-23 12:27:46 +0200

    initial commit

---
+++ mia-pecelj-be/src/main/java/mia/pecelj/be/MiaPeceljBeApplication.java
@@ -0,0 +1,13 @@
+package mia.pecelj.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaPeceljBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaPeceljBeApplication.class, args);
+	}
+
+}
---
+++ mia-pecelj-be/src/test/java/mia/pecelj/be/MiaPeceljBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mia.pecelj.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaPeceljBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
